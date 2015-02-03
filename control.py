0b9081bbdef127ff35a07f388c4863ef0e166e4a
Matthew Feng <netsec.phs@gmail.com>
2015-02-03 09:40:19 -0500

    lol

---
+++ control.py
@@ -0,0 +1,10 @@
+import serial;
+import curses;
+
+motors = serial.Serial("/dev/ttymxc3", 9600);
+stdscr = curses.initscr();
+while True:
+    c = stdscr.getch();
+    motors.write(c.upper());
+
+print "Done."
