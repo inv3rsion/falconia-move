006fab0fb45a74b40b902d4d9837bd2058189582
Matthew Feng <netsec.phs@gmail.com>
2015-02-04 07:50:09 -0500

    k

--- gpsnav.py
+++ gpsnav.py
@@ -14,7 +14,7 @@ FULL_TURN_TIME = 23; # TIME TAKES TO TURN 360, IN SECONDS
 TPOS_X = 1433;
 TPOS_Y = 408;
 DIST_THRESHOLD = 100;
-points = [(1472, 380), (1129, 350), (1001, 797)];
+points = [(1511, 411), (1102, 453)];
 
 def getGPS():
     try:
